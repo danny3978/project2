7f98ba2fcfb69efd9779dc7cc73c0e8ab0418f72
Robbie <robbie331@>
2024-07-25 18:55:24 +0900

    ãlevel_02_07 finish

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,42 +12,46 @@ public class App {
 
 
 
+        while(true) {
+            System.out.println("반지름으로 원의 넓이를 구하시겠습니까?");
+            System.out.println("yes or no");
 
-
-        while(true){
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
-
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char ch = sc.next().charAt(0);
-
-            try{
-                cl.calculate(num1, num2, ch);
-            } catch (ArithmeticException e){
-                System.out.println(e.getMessage());
-                System.out.println("값이 0이 출력됩니다. 제거해주세요.");
-            }
-
-
-
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            cl.removeResult(sc.next());
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            cl.inquiryResults(sc.next());
-
-
-            System.out.println();
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            if(sc.next().equals("exit")){
+            if (sc.next().equals("yes")) {
+                System.out.print("반지름을 입력하세요: ");
+                cl.calculateCircleArea(sc.nextInt());
                 break;
+            } else {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = sc.nextInt();
+
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = sc.nextInt();
+
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char ch = sc.next().charAt(0);
+
+                try {
+                    cl.calculate(num1, num2, ch);
+                } catch (ArithmeticException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("값이 0이 출력됩니다. 제거해주세요.");
+                }
+
+
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                cl.removeResult(sc.next());
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                cl.inquiryResults(sc.next());
+
+
+                System.out.println();
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                if (sc.next().equals("exit")) {
+                    break;
+                }
             }
         }
-
-
     }
 }
+
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,8 +8,13 @@ public class Calculator {
     private double total;
     List<Double> arr = new ArrayList<>();
 
+    final double PI = 3.14;
+    // 원주율은 변하지 않기 때문에 값이 변하지 않는 final을 씀
+    private double o;
+
+
     Calculator(){
-        this.arr.clear();
+
     }
 
     public void calculate(int num1, int num2, char ch) throws ArithmeticException{
@@ -59,7 +64,20 @@ public class Calculator {
                 System.out.print(all + " ");
             }
         }
-
-
     }
+
+
+    public void calculateCircleArea(int ban) {
+        setO(ban * ban * PI);
+        System.out.println(this.o);
+    }
+
+    public double getO(){
+        return o;
+    }
+    public void setO(double num1){
+        this.o = num1;
+    }
+
+
 }
