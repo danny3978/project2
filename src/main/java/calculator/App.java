c95efe0dec89d87b6b03f409b9969617d23913f0
Robbie <robbie331@>
2024-07-25 17:40:20 +0900

    level-02-04 finish

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,7 +9,6 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Double> arr = new ArrayList<>();
 
         Calculator cl = new Calculator();
 
@@ -36,26 +35,19 @@ public class App {
             }
 
 
-            double result;
-            result = cl.getTotal();
 
-
-            System.out.println("결과: " + result);
-
-
-            arr.add(result);
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if(sc.next().equals("remove")){
-                arr.remove(0);
-            }
+            cl.removeResult(sc.next());
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if(sc.next().equals("inquiry")){
-                for(double all : arr){
-                    System.out.print(all + " ");
-                }
-            }
+//            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+//            if(sc.next().equals("inquiry")){
+//                for(double all : arr){
+//                    System.out.print(all + " ");
+//                }
+//            }
+
+
             System.out.println();
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             if(sc.next().equals("exit")){
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package calculator;
 
+import com.sun.tools.javac.Main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Calculator {
     private double total;
 
@@ -16,6 +22,7 @@ public class Calculator {
                     break;
                 case '*':
                     total = num1 * num2;
+                    break;
                 case '/':
                     if(num2 == 0){
                         throw new ArithmeticException("0으로 나눌 수 없습니다.");
@@ -23,7 +30,9 @@ public class Calculator {
                     total = (double) num1 / num2;
                     break;
             }
+            System.out.println("결과: " + total);
             return total;
+
     }
 
     public double getTotal(){
@@ -33,5 +42,14 @@ public class Calculator {
     public double setTotal(double total){
         this.total = total;
         return this.total;
+    }
+
+    public void removeResult(String remove){
+        List<Double> arr = new ArrayList<>();
+        arr.add(total);
+        if(remove.equals("remove")){
+            arr.remove(0);
+        }
+
     }
 }
